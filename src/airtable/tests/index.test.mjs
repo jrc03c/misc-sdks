3c4af7036f95472a9d3115e2abe5e27bd790e04c
jrc03c <jrc03c@pm.me>
2025-10-27 13:13:51 -0500

    ...

---
+++ src/airtable/tests/index.test.mjs
@@ -0,0 +1,18 @@
+import { AirtableClient } from "../index.mjs"
+import { AirtableBase } from "../base/index.mjs"
+import { expect, test } from "@jrc03c/fake-jest"
+
+test("AirtableClient", () => {
+  expect(() => new AirtableClient()).toThrow()
+
+  const client = new AirtableClient({ token: "foobar" })
+  expect(client.apiVersion).toBe(0)
+  expect(client.baseUrl).toBe("https://api.airtable.com/v0")
+  expect(client.exponentialBackoffHelper.ms).toBe(1000 / 50)
+  expect(client.token).toBe("foobar")
+
+  const base = client.getBase("whatevs")
+  expect(base instanceof AirtableBase).toBe(true)
+  expect(base.client).toBe(client)
+  expect(base.id).toBe("whatevs")
+})
