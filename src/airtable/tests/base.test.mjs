4629c3dd2088f972bb089c5051d1e1b7d3eb8d99
jrc03c <jrc03c@pm.me>
2025-10-27 13:20:37 -0500

    ...

--- src/airtable/tests/base.test.mjs
+++ src/airtable/tests/base.test.mjs
@@ -1,6 +1,8 @@
 import { AirtableClient } from "../index.mjs"
 import { AirtableClientResponse } from "../response.mjs"
+import { AirtableTable } from "../table/index.mjs"
 import { expect, test } from "@jrc03c/fake-jest"
+import { isEqual } from "@jrc03c/js-math-tools"
 import process from "node:process"
 
 if (typeof process.env.AIRTABLE_API_TOKEN === "undefined") {
@@ -21,10 +23,20 @@ test("AirtableBase", async () => {
   })
 
   const base = client.getBase(process.env.AIRTABLE_BASE_ID)
-  const response = await base.getTableSchemas()
+  const response1 = await base.getTableSchemas()
 
-  expect(response instanceof AirtableClientResponse).toBe(true)
-  expect(response.status).toBe(200)
-  expect(response.json.tables.length).toBe(1)
-  expect(response.json.tables[0].id).toBe(process.env.AIRTABLE_TABLE_ID)
+  expect(response1 instanceof AirtableClientResponse).toBe(true)
+  expect(response1.status).toBe(200)
+  expect(response1.json.tables.length).toBe(1)
+  expect(response1.json.tables[0].id).toBe(process.env.AIRTABLE_TABLE_ID)
+
+  const response2 = await base.getTableSchema(process.env.AIRTABLE_TABLE_ID)
+  expect(response2.status).toBe(200)
+  expect(isEqual(response2.json, response1.json.tables[0])).toBe(true)
+
+  const table = base.getTable(process.env.AIRTABLE_TABLE_ID)
+  expect(table instanceof AirtableTable).toBe(true)
+  expect(table.base).toBe(base)
+  expect(table.client).toBe(client)
+  expect(table.id).toBe(process.env.AIRTABLE_TABLE_ID)
 })
