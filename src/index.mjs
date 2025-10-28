56014267f25e54daa56f5f3661922660532d7163
jrc03c <jrc03c@pm.me>
2025-10-28 11:15:12 -0500

    ...

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,11 @@
+import { AirtableClient, AirtableClientResponse } from "./airtable/index.mjs"
 import { BaseClient, BaseClientResponse } from "./base/index.mjs"
 import { MailchimpClient, MailchimpClientResponse } from "./mailchimp/index.mjs"
 import { MailTmClient, MailTmClientResponse } from "./mail-tm/index.mjs"
 
 export {
+  AirtableClient,
+  AirtableClientResponse,
   BaseClient,
   BaseClientResponse,
   MailchimpClient,
