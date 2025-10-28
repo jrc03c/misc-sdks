f712dc5a1e1bb4fa143500a1bcf0c417aa6762b6
jrc03c <jrc03c@pm.me>
2025-10-28 07:29:38 -0500

    ...

--- src/airtable/tests/table.test.mjs
+++ src/airtable/tests/table.test.mjs
@@ -1,6 +1,6 @@
+import { afterAll, expect, test } from "@jrc03c/fake-jest"
 import { AirtableClient } from "../index.mjs"
 import { AirtableClientResponse } from "../response.mjs"
-import { expect, test } from "@jrc03c/fake-jest"
 import process from "node:process"
 
 if (typeof process.env.AIRTABLE_API_TOKEN === "undefined") {
@@ -15,13 +15,39 @@ if (typeof process.env.AIRTABLE_TABLE_ID === "undefined") {
   throw new Error("The environment variable `AIRTABLE_TABLE_ID` is undefined!")
 }
 
-test("AirtableTable", async () => {
-  const client = new AirtableClient({
-    token: process.env.AIRTABLE_API_TOKEN,
-  })
+class TestRecord {
+  fields = {
+    Name: "",
+    Notes: "",
+    Assignee: "",
+    Status: "",
+    DueDate: "",
+  }
 
-  const base = client.getBase(process.env.AIRTABLE_BASE_ID)
-  const table = base.getTable(process.env.AIRTABLE_TABLE_ID)
+  constructor(data) {
+    data = data || {}
+    this.fields.Name = data.Name || this.fields.Name
+    this.fields.Notes = data.Notes || this.fields.Notes
+    this.fields.Assignee = data.Assignee || this.fields.Assignee
+    this.fields.Status = data.Status || this.fields.Status
+    this.fields.DueDate = data.DueDate || this.fields.DueDate
+  }
+}
+
+const client = new AirtableClient({
+  token: process.env.AIRTABLE_API_TOKEN,
+})
+
+const base = client.getBase(process.env.AIRTABLE_BASE_ID)
+const table = base.getTable(process.env.AIRTABLE_TABLE_ID)
+
+const recordsIdsToDelete = []
+
+afterAll(async () => {
+  await table.deleteRecords(recordsIdsToDelete)
+})
+
+test("AirtableTable", async () => {
   const recordIds = []
 
   await (async () => {
@@ -36,5 +62,44 @@ test("AirtableTable", async () => {
     const response = await table.getRecord(recordIds[0])
     expect(response.status).toBe(200)
     expect(response.json.id).toBe(recordIds[0])
+  })()
+
+  await (async () => {
+    const records = [
+      new TestRecord({
+        Name: "Find the meaning of life, the universe, and everything",
+        Notes: Math.random().toString(),
+        Assignee: "Gertrude",
+        Status: "In progress",
+        DueDate: "3/15/2025",
+      }),
+      new TestRecord({
+        Name: "Clean your room",
+        Notes: Math.random().toString(),
+        Assignee: "Hilda",
+        Status: "Todo",
+        DueDate: "6/9/2025",
+      }),
+    ]
+
+    const response = await table.createRecords(records)
+    expect(response.status).toBe(200)
+    expect(response.json.records.length).toBe(records.length)
+
+    for (let i = 0; i < records.length; i++) {
+      const rtrue = records[i]
+      const rpred = response.json.records[i]
+      const keys = Object.keys(rtrue.fields).filter(v => v !== "DueDate")
+
+      for (let j = 0; j < keys.length; j++) {
+        const key = keys[j]
+        expect(rpred.fields[key]).toBe(rtrue.fields[key])
+      }
+    }
+
+    expect(response.json.records[0].fields["DueDate"]).toBe("2025-03-15")
+    expect(response.json.records[1].fields["DueDate"]).toBe("2025-06-09")
+
+    recordsIdsToDelete.push(...response.json.records.map(r => r.id))
   })()
 })
