8827fb9e010d110a29be6eb908005dd511a96bd6
jrc03c <jrc03c@pm.me>
2025-11-06 17:13:50 -0600

    ...

--- src/airtable/table-ref/methods/get-schema.test.mjs
+++ src/airtable/table-ref/methods/get-schema.test.mjs
@@ -4,13 +4,11 @@ import { isEqual } from "@jrc03c/js-math-tools"
 import { tableRef } from "./_set_up_and_tear_down_tests.mjs"
 
 test("AirtableTableRef.getSchema", async () => {
-  await (async () => {
-    const response1 = await tableRef.getSchema()
-    expect(response1 instanceof AirtableClientResponse).toBe(true)
-    expect(response1.status).toBe(200)
+  const response1 = await tableRef.getSchema()
+  expect(response1 instanceof AirtableClientResponse).toBe(true)
+  expect(response1.status).toBe(200)
 
-    const response2 = await tableRef.baseRef.getTableSchema(tableRef.id)
-    expect(response2.status).toBe(200)
-    expect(isEqual(response1.json, response2.json)).toBe(true)
-  })()
+  const response2 = await tableRef.baseRef.getTableSchema(tableRef.id)
+  expect(response2.status).toBe(200)
+  expect(isEqual(response1.json, response2.json)).toBe(true)
 })
